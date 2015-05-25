1ec16ab9c1a06dc83135b9266506047d2e819955
zimmed <dzimmelman@gmail.com>
2015-05-25 16:21:56 -0400

    Added use of res.write

--- routes/submit.js
+++ routes/submit.js
@@ -11,13 +11,13 @@ router.get('/', function (req, res) {
         assign = req.query.assign,
         apath = req.query.path,
         date = +new Date();
-    res.send('Submission received. Processing...\n');
+    res.write('Submission received. Processing...\n');
     if (!user || !assign || !apath ||
         !fs.existsSync(apath) || !(astats = submission.getStats(assign))) {
         submission.addError('Bad submission request from: ' + user, req.query);
         res.send(s + '\nSubmission failed! 400: Bad Request' + s + '\n\n');
     } else {
-        res.send('Saving submission (may take a minute)...\n');
+        res.write('Saving submission (may take a minute)...\n');
         submission.addSubmission(astats, user, assign, apath, date).then(function (sub) {
             s += 'Submission for `' + sub.assign + '` Succeeded!\n';
             s += (sub.count) ? 'Re-submission #' + sub.count : 'First submission';
